0d89af226d982d0e58219f5978f5ed0125d4cafe
Victor Brakauskas <victor.brakauskas@gmail.com>
2017-06-07 20:54:44 -0500

    deploy script made a lil less shitty

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,8 @@
 
 THEMEURL="https://github.com/brakaus1/hugo-theme-casper"
 
-cd blog
-rm -rf public/
-cd themes && git clone $THEMEURL
+rm -rf blog/public/
+cd blog/themes && git clone https://github.com/brakaus1/hugo-theme-casper
 cd ..
 hugo --theme=hugo-theme-casper
-cd public
-aws s3 cp . s3://$AWS_S3_BUCKET/ --recursive
-
+aws s3 cp public/ s3://$AWS_S3_BUCKET/ --recursive
